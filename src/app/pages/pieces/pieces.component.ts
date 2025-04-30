8345d26723cc4cecdff964d11dde5f0d69ebb04c
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-04-30 10:56:56 +0300

    add sort dropdown to the pieces page

--- src/app/pages/pieces/pieces.component.ts
+++ src/app/pages/pieces/pieces.component.ts
@@ -1,16 +1,32 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {PieceCardComponent, PieceService} from '@entities/piece';
-import {ContainerComponent, TypographyComponent} from '@shared/components';
+import {
+	ContainerComponent,
+	DropdownComponent,
+	DropdownItemComponent,
+	TypographyComponent,
+} from '@shared/components';
 
 @Component({
 	selector: 'app-pieces-page',
 	templateUrl: './pieces.component.html',
-	imports: [ContainerComponent, PieceCardComponent, TypographyComponent],
+	imports: [
+		ContainerComponent,
+		PieceCardComponent,
+		TypographyComponent,
+		DropdownComponent,
+		DropdownItemComponent,
+	],
 })
 export class PiecesPageComponent {
 	readonly pieces = inject(PieceService);
+	readonly sortDropdownIsOpen = signal<boolean>(false);
 
 	ngOnInit(): void {
 		this.pieces.fetchAll().subscribe();
+	}
+
+	toggleSortDropdownIsOpen() {
+		this.sortDropdownIsOpen.update(value => !value);
 	}
 }
