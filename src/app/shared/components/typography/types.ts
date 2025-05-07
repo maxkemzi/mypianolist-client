f5e8f36b782cf025759f3cb197016bf182927732
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-05-07 18:21:51 +0300

    add 'inherit' variant for typography component

--- src/app/shared/components/typography/types.ts
+++ src/app/shared/components/typography/types.ts
@@ -1,7 +1,7 @@
-type Variant = 'body2' | 'body1' | 'h3' | 'h2' | 'h1';
+type Variant = 'inherit' | 'body2' | 'body1' | 'h3' | 'h2' | 'h1';
 type Tag = 'h1' | 'h2' | 'h3' | 'p';
-type Weight = 'normal' | 'medium' | 'semibold' | 'bold';
-type Size = 'sm' | 'base' | '2xl' | '4xl' | '5xl';
-type Color = 'primary' | 'text' | 'danger';
+type Weight = 'inherit' | 'normal' | 'medium' | 'semibold' | 'bold';
+type Size = 'inherit' | 'sm' | 'base' | '2xl' | '4xl' | '5xl';
+type Color = 'inherit' | 'primary' | 'text' | 'danger';
 
 export type {Variant, Tag, Weight, Size, Color};
--- src/app/shared/components/typography/typography.component.ts
+++ src/app/shared/components/typography/typography.component.ts
@@ -1,4 +1,3 @@
-import {CommonModule} from '@angular/common';
 import {Component, computed, ElementRef, inject, input} from '@angular/core';
 import {ClassMergeDirective} from '@shared/lib';
 import {twJoin} from 'tailwind-merge';
@@ -7,12 +6,12 @@ import {Color, Size, Tag, Variant, Weight} from './types';
 @Component({
 	selector: 'h1, h2, h3, h4, h5, h6, p, span, [appTypography]',
 	templateUrl: './typography.component.html',
-	imports: [CommonModule],
+	imports: [],
 })
 export class TypographyComponent extends ClassMergeDirective {
 	private readonly el = inject(ElementRef);
 	readonly variant = input<Variant>();
-	readonly color = input<Color>('text');
+	readonly color = input<Color>();
 	readonly size = input<Size>();
 	readonly weight = input<Weight>();
 
@@ -30,12 +29,14 @@ export class TypographyComponent extends ClassMergeDirective {
 
 	protected override defaultClass(): string {
 		const COLOR_TO_CLASSES_MAPPING: Record<Color, string> = {
+			inherit: 'text-inherit',
 			text: 'text-text',
 			primary: 'text-primary',
 			danger: 'text-danger',
 		};
 
 		const SIZE_TO_CLASSES_MAPPING: Record<Size, string> = {
+			inherit: 'text-[length:inherit]',
 			sm: 'text-sm',
 			base: 'text-base',
 			'2xl': 'text-2xl',
@@ -44,6 +45,7 @@ export class TypographyComponent extends ClassMergeDirective {
 		};
 
 		const WEIGHT_TO_CLASSES_MAPPING: Record<Weight, string> = {
+			inherit: 'font-[inherit]',
 			normal: 'font-normal',
 			medium: 'font-medium',
 			semibold: 'font-semibold',
@@ -52,33 +54,44 @@ export class TypographyComponent extends ClassMergeDirective {
 
 		const VARIANT_TO_PROPS_MAPPING: Record<
 			Variant,
-			{size: Size; weight: Weight}
+			{size: Size; color: Color; weight: Weight}
 		> = {
+			inherit: {
+				size: 'inherit',
+				color: 'inherit',
+				weight: 'inherit',
+			},
 			body2: {
 				size: 'sm',
+				color: 'text',
 				weight: 'normal',
 			},
 			body1: {
 				size: 'base',
+				color: 'text',
 				weight: 'normal',
 			},
 			h3: {
 				size: '2xl',
+				color: 'text',
 				weight: 'semibold',
 			},
 			h2: {
 				size: '4xl',
+				color: 'text',
 				weight: 'semibold',
 			},
 			h1: {
 				size: '5xl',
+				color: 'text',
 				weight: 'bold',
 			},
 		};
 
-		const {size, weight} = VARIANT_TO_PROPS_MAPPING[this.finalVariant()];
+		const {size, color, weight} =
+			VARIANT_TO_PROPS_MAPPING[this.finalVariant()];
 
-		const colorClasses = COLOR_TO_CLASSES_MAPPING[this.color()];
+		const colorClasses = COLOR_TO_CLASSES_MAPPING[this.color() || color];
 		const sizeClasses = SIZE_TO_CLASSES_MAPPING[this.size() || size];
 		const weightClasses = WEIGHT_TO_CLASSES_MAPPING[this.weight() || weight];
 
