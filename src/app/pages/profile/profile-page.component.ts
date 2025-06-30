d871450cfe91027b8b699b46928fcce599f35765
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2025-06-30 08:07:23 +0300

    use single loader and error fallback for all profile requests

--- src/app/pages/profile/profile-page.component.ts
+++ src/app/pages/profile/profile-page.component.ts
@@ -1,4 +1,17 @@
-import {Component, DestroyRef, inject, input, OnInit} from '@angular/core';
+import {
+	Component,
+	DestroyRef,
+	inject,
+	input,
+	OnInit,
+	signal,
+} from '@angular/core';
+import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
+import {RouterLink} from '@angular/router';
+import {FavoriteComposerCardComponent} from '@entities/composer';
+import {FavoritePieceCardComponent} from '@entities/piece';
+import {FetchFavoriteComposersService} from '@features/composer/fetch-favorite';
+import {FetchFavoritePiecesService} from '@features/piece/fetch-favorite';
 import {FetchPieceStatsService} from '@features/piece/fetch-stats';
 import {FetchUserProfileService} from '@features/user/profile/fetch';
 import {
@@ -6,14 +19,8 @@ import {
 	ContainerComponent,
 	TypographyComponent,
 } from '@shared/components';
+import {catchError, finalize, forkJoin, of} from 'rxjs';
 import {StatusLabelComponent} from './components/status-label/status-label.component';
-import {FetchFavoritePiecesService} from '@features/piece/fetch-favorite';
-import {FavoritePieceCardComponent} from '@entities/piece';
-import {RouterLink} from '@angular/router';
-import {FetchFavoriteComposersService} from '@features/composer/fetch-favorite';
-import {FavoriteComposerCardComponent} from '@entities/composer';
-import {forkJoin} from 'rxjs';
-import {takeUntilDestroyed} from '@angular/core/rxjs-interop';
 
 @Component({
 	selector: 'app-profile-page',
@@ -38,48 +45,46 @@ export class ProfilePageComponent implements OnInit {
 	private readonly destroyRef = inject(DestroyRef);
 
 	readonly username = input<string>();
-	readonly profile = {
-		data: this.fetchUserProfile.data,
-		isLoading: this.fetchUserProfile.isLoading,
-		hasError: this.fetchUserProfile.hasError,
-	};
-	readonly stats = {
-		data: this.fetchPieceStats.data,
-		isLoading: this.fetchPieceStats.isLoading,
-		hasError: this.fetchPieceStats.hasError,
-	};
-	readonly pieces = {
-		data: this.fetchFavoritePieces.data,
-		isLoading: this.fetchFavoritePieces.isLoading,
-		hasError: this.fetchFavoritePieces.hasError,
-	};
-	readonly composers = {
-		data: this.fetchFavoriteComposers.data,
-		isLoading: this.fetchFavoriteComposers.isLoading,
-		hasError: this.fetchFavoriteComposers.hasError,
-	};
+	readonly isLoading = signal<boolean>(false);
+	readonly hasError = signal<boolean>(false);
+	readonly profile = {data: this.fetchUserProfile.data};
+	readonly stats = {data: this.fetchPieceStats.data};
+	readonly pieces = {data: this.fetchFavoritePieces.data};
+	readonly composers = {data: this.fetchFavoriteComposers.data};
 
 	ngOnInit(): void {
+		this.isLoading.set(true);
+		this.hasError.set(false);
+
 		const username = this.username();
-		if (!username) {
-			forkJoin([
-				this.fetchUserProfile.fetchByAuth(),
-				this.fetchPieceStats.fetchByAuth(),
-				this.fetchFavoritePieces.fetchByAuth(),
-				this.fetchFavoriteComposers.fetchByAuth(),
-			])
-				.pipe(takeUntilDestroyed(this.destroyRef))
-				.subscribe();
-		} else {
-			forkJoin([
-				this.fetchUserProfile.fetchByUsername(username),
-				this.fetchPieceStats.fetchByUsername(username),
-				this.fetchFavoritePieces.fetchByUsername(username),
-				this.fetchFavoriteComposers.fetchByUsername(username),
-			])
-				.pipe(takeUntilDestroyed(this.destroyRef))
-				.subscribe();
-		}
+		let fetch;
+
+		fetch = username
+			? forkJoin([
+					this.fetchUserProfile.fetchByUsername(username),
+					this.fetchPieceStats.fetchByUsername(username),
+					this.fetchFavoritePieces.fetchByUsername(username),
+					this.fetchFavoriteComposers.fetchByUsername(username),
+				])
+			: forkJoin([
+					this.fetchUserProfile.fetchByAuth(),
+					this.fetchPieceStats.fetchByAuth(),
+					this.fetchFavoritePieces.fetchByAuth(),
+					this.fetchFavoriteComposers.fetchByAuth(),
+				]);
+
+		fetch
+			.pipe(
+				catchError(() => {
+					this.hasError.set(true);
+					return of();
+				}),
+				finalize(() => {
+					this.isLoading.set(false);
+				}),
+				takeUntilDestroyed(this.destroyRef),
+			)
+			.subscribe();
 	}
 
 	formatJoinedAt(joinedAt: string) {
